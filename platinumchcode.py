8920846453af8a354b9feea7ee840ea182319027
unknown <damanhuri.hilman@gmail.com>
2024-04-06 01:37:36 +0700

    third commit

--- platinumchcode.py
+++ platinumchcode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc007c75bbff60902246fee9bdded3f898ec9f24287a0ff62afeedc4cfeb8376
-size 8873
+oid sha256:393f9fe17635ca0d03cdecd2b1b5fe322a06da3a5da22b8133f803e6c66b0e5b
+size 9341
