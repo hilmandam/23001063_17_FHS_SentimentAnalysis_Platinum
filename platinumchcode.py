fbc98a70e334bc51b39b710038431181c10a5c52
satriasaid <satriasaid@gmail.com>
2024-04-06 01:41:47 +0700

    Satria: fixed code issue

--- platinumchcode.py
+++ platinumchcode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc007c75bbff60902246fee9bdded3f898ec9f24287a0ff62afeedc4cfeb8376
-size 8873
+oid sha256:7abeff0a7828de2c3104a18ab298c96734291699bda66a48d385aa912be152ae
+size 8871
