595df649e24285f91b0f4ef048f18d03c733a671
satriasaid <satriasaid@gmail.com>
2024-04-07 00:33:56 +0700

    Satria: code edit on NN Sentiment from File

--- platinumchcode.py
+++ platinumchcode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88518da174ce8a1b7c777a3b7d6c35ac3320a0a735bb20ad4e68a282343a1619
-size 9339
+oid sha256:7a64f0a6b16abfb67eb30a89d2dfeb82c8d517ad3ed1a8fc5cd9fa84ed63d720
+size 9675
